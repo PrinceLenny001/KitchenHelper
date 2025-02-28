5b7f3043b026273389d9e20b5994fcaf809bfb32
PrinceLenny001 <andy.leneway@gmail.com>
2025-02-28 09:48:37 -0500

    Remove login screen by bypassing authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "@/app/globals.css";
 import "react-toastify/dist/ReactToastify.css";
-import { TRPCReactProvider } from "@/lib/trpc/react";
 import { Metadata } from "next";
 import ClientProvider from "@/components/ClientProvider";
-import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import { ThemeAwareToast } from "@/components/theme/ThemeAwareToast";
-import { AppProviders } from "@/lib/context/AppProviders";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
 
 export const metadata: Metadata = {
@@ -25,18 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider defaultTheme="system" enableSystem>
-          <ClientProvider>
-            <TRPCReactProvider>
-              <AppProviders>
-                <ErrorBoundary>
-                  {children}
-                </ErrorBoundary>
-              </AppProviders>
-              <ThemeAwareToast />
-            </TRPCReactProvider>
-          </ClientProvider>
-        </ThemeProvider>
+        <ClientProvider>
+          <ErrorBoundary>
+            {children}
+          </ErrorBoundary>
+          <ThemeAwareToast />
+        </ClientProvider>
       </body>
     </html>
   );
